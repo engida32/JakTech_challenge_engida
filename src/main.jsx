14c294a21cab5f385677570861228211f8fb1422
Engida Lishan <engida32lishan@gmail.com>
2023-04-08 20:08:06 +0300

    react-query init setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,22 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 import App from "./App";
 import { theme } from "./theme/theme";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
